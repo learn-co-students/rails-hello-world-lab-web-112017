32eaef38d9b900c565926a296dc12081849a02b6
Flatiron School <leslie.hsu@flatironschool.com>
2017-12-15 14:46:26 -0500

    Done.

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Helloworldlab
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+#I added this per guidance upon bundle update
+    config.web_console.development_only = false
+
   end
 end
